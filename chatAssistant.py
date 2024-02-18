abd7477c3bd3fd3dac7f3c637647a5b17900369e
brutakashi <brutakashi@gmail.com>
2024-02-18 16:53:11 -0300

    Update chatAssistant.py

--- chatAssistant.py
+++ chatAssistant.py
@@ -123,7 +123,7 @@ def process_message_with_citations(message):
     return full_response
 
 # Interface do chat
-st.subheader("ANÁLISE DE ITENS")
+st.subheader("ANÁLISE DE ITENSS")
 #st.write("Este chat usa a API da OpenAI para gerar respostas.")
 
 # Só vai mostrar o chat se for iniciado
